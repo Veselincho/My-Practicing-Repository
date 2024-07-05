b9e6afb57b2edb45ef56e6f52420933f548b21b1
Veselin Ivanov <veso42srma@gmail.com>
2024-07-05 12:26:34 +0300

    removed JavaScript

--- JavaScript/JS-Fundamentals/01-sum-first.js
+++
@@ -1,9 +0,0 @@
-function solve(input) {
-    let firstElement = input.shift();
-    let lastElement = input.pop();
-    let result = firstElement + lastElement;
-
-    console.log(result);
-}
-
-solve([20, 30, 40]);
--- JavaScript/Unit-Testing-with-JS/commonJS-modules/index.js
+++
@@ -1,5 +0,0 @@
-// Import commoon js module
-let calculator = require('./calculator');
-let {subtract, sum} = require('./calculator');
-
-console.log(calculator.sum(5, 10));
