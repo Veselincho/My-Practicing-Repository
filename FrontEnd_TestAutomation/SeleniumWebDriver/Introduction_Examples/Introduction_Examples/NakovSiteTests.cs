89361bc60ae65c865ad22e0d2320abb92f8036f7
Veselin Ivanov <veso42srma@gmail.com>
2024-07-07 14:56:57 +0300

    NavbarTests added

--- FrontEnd_TestAutomation/SeleniumWebDriver/Introduction_Examples/Introduction_Examples/NakovSiteTests.cs
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Introduction_Examples/Introduction_Examples/NakovSiteTests.cs
@@ -1,25 +1,27 @@
 ﻿global using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
 
 namespace Introduction_Examples
 {
-    public class FirstTest
+    public class NakovWebsite
     {
         private IWebDriver driver;
         private string url = "https://nakov.com/";
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
 
         [TearDown]
         public void TearDown()
         {
             driver.Quit();
             //driver.Dispose();
-        }
-
-        [SetUp]
-        public void SetUp()
-        {
-            driver = new ChromeDriver();
         }
 
         [Test]
@@ -40,6 +42,37 @@ namespace Introduction_Examples
             var attributeValue = element.GetAttribute("placeholder");
             Assert.That(attributeValue, Is.EqualTo("Search this site"));
         }
-    }
 
+        [Test]
+        public void NavBarLIElementsAreHiddenWithLessThan1153pxWidth()
+        {
+            driver.Navigate().GoToUrl(url);
+            System.Drawing.Size windowSize = new System.Drawing.Size(1152, 300);
+            driver.Manage().Window.Size = windowSize;
+            Thread.Sleep(3000);
+
+            var navBar = driver.FindElement(By.CssSelector("body > header > nav"));
+            Assert.False(navBar.Displayed);
+
+            // the media Queries I found
+            // only screen and (max-width: 1152px) NON Visible
+            // only screen and (max-width: 1153px) Visible
+        }
+
+        [Test]
+        public void NavBarLIElementsAreVisibleWhenWidthIsGreaterThan1152()
+        {
+            driver.Navigate().GoToUrl(url);
+            System.Drawing.Size windowSize = new System.Drawing.Size(2000, 353);
+            driver.Manage().Window.Size = windowSize;
+            Thread.Sleep(10000);
+
+            var navBar = driver.FindElement(By.CssSelector("body > header > nav"));
+            Assert.True(navBar.Displayed);
+
+            // the conditions for media Queries I found:
+            // only screen and (max-width: 1152px) NON Visible
+            // only screen and (max-width: 1153px) Visible
+        }
+    }
 }
