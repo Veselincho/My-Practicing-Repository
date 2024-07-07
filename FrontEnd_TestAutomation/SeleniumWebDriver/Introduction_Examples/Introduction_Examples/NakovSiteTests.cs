64d2e66212290324e9533a35a1fdcfa198ecb10b
Veselin Ivanov <veso42srma@gmail.com>
2024-07-07 12:18:27 +0300

    added new test file

---
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Introduction_Examples/Introduction_Examples/NakovSiteTests.cs
@@ -0,0 +1,35 @@
+﻿global using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Introduction_Examples
+{
+    public class FirstTest
+    {
+        private IWebDriver driver;
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            //driver.Dispose();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver(); 
+        }
+
+        [Test]
+        public void GetTitle()
+        {
+            string url = "https://nakov.com/";
+            driver.Navigate().GoToUrl(url);
+            string title = driver.Title;
+            Console.WriteLine(title);
+            Assert.That(title, Is.EqualTo("Svetlin Nakov - Svetlin Nakov – Official Web Site and Blog"));
+        }
+    }
+
+}
