a0421dcd2356b33a898dfd3dd2a7a4da81663d44
Veselin Ivanov <veso42srma@gmail.com>
2024-07-10 18:58:58 +0300

    added new test file

---
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Introduction_Examples/Introduction_Examples/locatorsPractice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Introduction_Examples
+{
+    internal class locatorsPractice
+    {
+        private string baseUrl = "file:///C:/Users/VesoPC/Desktop/My-Practicing-Repository/FrontEnd_TestAutomation/SeleniumWebDriver/Introduction_Examples/simpleForm";
+    }
+}
