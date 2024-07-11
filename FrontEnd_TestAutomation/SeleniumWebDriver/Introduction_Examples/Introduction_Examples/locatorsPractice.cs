b0c0e6f31f21bd626444cc71c428e786ae580373
Veselin Ivanov <veso42srma@gmail.com>
2024-07-11 20:15:28 +0300

    nth

--- FrontEnd_TestAutomation/SeleniumWebDriver/Introduction_Examples/Introduction_Examples/locatorsPractice.cs
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Introduction_Examples/Introduction_Examples/locatorsPractice.cs
@@ -7,48 +7,30 @@ namespace Introduction_Examples
     [TestFixture]
     public class LocatorsPractice
     {
-        private string baseUrl = "file:///C:/Users/VesoPC/Desktop/My-Practicing-Repository/FrontEnd_TestAutomation/SeleniumWebDriver/Introduction_Examples/simpleForm";
+        private string baseUrl = "file:///C:/Users/VesoPC/Desktop/My-Practicing-Repository/FrontEnd_TestAutomation/SeleniumWebDriver/Introduction_Examples/simpleForm/Locators.html";
 
         private IWebDriver driver;
 
         [OneTimeSetUp]
-        public void OneTimeSetup()
+        public void OneTimeSetup() // nodeJS playwright analog => beforeAll()
         {
-            // One-time setup code (runs once before any tests in this fixture)
             driver = new ChromeDriver();
             driver.Navigate().GoToUrl(baseUrl);
-            // Additional setup code if needed
         }
 
-        [OneTimeTearDown]
+        [OneTimeTearDown] // nodeJS playwright analog => afterAll()
         public void OneTimeTearDown()
         {
-            // One-time teardown code (runs once after all tests in this fixture)
             driver.Quit();
-        }
-
-        [SetUp]
-        public void Setup()
-        {
-            // Setup code that runs before each test method
-            // For example, logging in or navigating to a specific page
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            // Teardown code that runs after each test method
-            // For example, logging out or cleaning up resources
         }
 
         [Test]
         public void ExampleTest()
         {
-            // Example test method
-            // Use driver to interact with the web page and perform assertions
+
         }
 
-        // Add more test methods as needed
+        
 
     }
 }
