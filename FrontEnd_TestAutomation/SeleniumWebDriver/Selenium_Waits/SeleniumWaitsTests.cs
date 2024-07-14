89da467df530a52beb621d23d667a24445e3788e
Veselin Ivanov <veso42srma@gmail.com>
2024-07-14 21:30:36 +0300

    modified main structure ready for tests

--- FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_Waits/SeleniumWaitsTests.cs
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_Waits/SeleniumWaitsTests.cs
@@ -1,23 +1,34 @@
 global using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+
 namespace Selenium_Waits
 {
+    [TestFixture]
     public class SeleniumWaits
     {
-        string url = "https://www.selenium.dev/selenium/web/dynamic.html";
+        private IWebDriver driver;
+        private readonly string url = "https://www.selenium.dev/selenium/web/dynamic.html";
 
         [SetUp] // beforeEach()
         public void Setup()
         {
-            IWebDriver driver = new ChromeDriver();
-            
+            driver = new ChromeDriver();
+            var options = new ChromeOptions();
         }
 
         [Test]
         public void Test1()
         {
+            driver.Navigate().GoToUrl(url);
             Assert.Pass();
+        }
+
+        [TearDown] // afterEach()
+        public void Teardown()
+        {
+            driver.Quit();
+            driver.Dispose();
         }
     }
 }
