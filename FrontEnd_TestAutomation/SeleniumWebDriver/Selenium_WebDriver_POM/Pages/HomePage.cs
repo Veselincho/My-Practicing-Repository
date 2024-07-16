40f2fa5af822baaa4046d9a9d80d9b106f8bbaea
Veselin Ivanov <veso42srma@gmail.com>
2024-07-16 12:35:25 +0300

    homePage done

--- FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_WebDriver_POM/Pages/HomePage.cs
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_WebDriver_POM/Pages/HomePage.cs
@@ -8,6 +8,15 @@ namespace StudentsRegistryPOM.Pages
         {
         }
 
+        public override string PageUrl => "http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com:82/";
 
+        public IWebElement StudentsCountElement => driver.FindElement(By.CssSelector("body > p > b"));
+
+        public int StudentsCount()
+        {
+            string studentsCountString = this.StudentsCountElement.Text;
+
+            return int.Parse(studentsCountString);
+        }
     }
 }
