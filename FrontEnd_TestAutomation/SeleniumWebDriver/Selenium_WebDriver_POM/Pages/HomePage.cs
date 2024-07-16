22fc200ed962376a46aa49d3d213bd4b4a0e40d0
Veselin Ivanov <veso42srma@gmail.com>
2024-07-16 12:28:22 +0300

    added homePage.cs

---
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_WebDriver_POM/Pages/HomePage.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace StudentsRegistryPOM.Pages
+{
+    public class HomePage : BasePage
+    {
+        public HomePage(IWebDriver driver) : base(driver) //calls the base constructor to initialize the driver
+        {
+        }
+
+
+    }
+}
