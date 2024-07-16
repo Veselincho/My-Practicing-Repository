761d4d89e657bba091baddeab757030511228fe5
Veselin Ivanov <veso42srma@gmail.com>
2024-07-16 12:07:26 +0300

    getting started with POM

---
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_WebDriver_POM/Pages/BasePage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace StudentsRegistryPOM.Pages
+{
+    public class BasePage
+    {
+        protected readonly IWebDriver driver;
+        public virtual string PageUrl { get; }
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);  
+        }
+
+        
+      
+
+
+    }
+}
