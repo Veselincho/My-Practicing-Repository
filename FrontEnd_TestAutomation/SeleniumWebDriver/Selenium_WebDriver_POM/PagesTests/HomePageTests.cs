b34c99731cb2fc79cb71b2854d70a67a7b667c6a
Veselin Ivanov <veso42srma@gmail.com>
2024-07-17 23:30:44 +0300

    first test added

--- FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_WebDriver_POM/PagesTests/HomePageTests.cs
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_WebDriver_POM/PagesTests/HomePageTests.cs
@@ -1,6 +1,17 @@
-﻿namespace StudentsRegistryPOM.PagesTests
+﻿using StudentsRegistryPOM.Pages;
+
+namespace StudentsRegistryPOM.PagesTests
 {
-    public class HomePageTests
+    public class HomePageTests : BaseTests
     {
+        [Test]
+        public void TestHomePageContent()
+        {
+            var page = new HomePage(driver);
+            page.OpenPage();
+
+            Assert.AreEqual(driver.Title, page.GetPageTitle());
+            
+        }
     }
 }
