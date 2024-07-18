e5b117a6d069d77b5dd366d1e630002eada812a8
Veselin Ivanov <veso42srma@gmail.com>
2024-07-18 15:27:24 +0300

    completed test coverage for file

--- FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_WebDriver_POM/PagesTests/HomePageLinks.cs
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_WebDriver_POM/PagesTests/HomePageLinks.cs
@@ -10,16 +10,18 @@ namespace StudentsRegistryPOM.PagesTests
         {
             var homePage = new HomePage(driver);
             homePage.OpenPage();
+            homePage.HomeLink.Click();
             Assert.IsTrue(homePage.IsPageOpen());
 
             var addStudentPage = new AddStudentPage(driver);
             addStudentPage.OpenPage();
+            addStudentPage.AddStudentLink.Click();
             Assert.IsTrue(addStudentPage.IsPageOpen());  
 
             ViewStudentsPage viewStudentsPage = new ViewStudentsPage(driver);   
             viewStudentsPage.OpenPage();
+            viewStudentsPage.ViewStudentLink.Click();
             Assert.True(viewStudentsPage.IsPageOpen());
         }
-
     }
 }
