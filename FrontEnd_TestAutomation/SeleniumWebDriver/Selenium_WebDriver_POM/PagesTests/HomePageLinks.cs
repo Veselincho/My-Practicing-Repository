27da36180953c55da2ab4a7a6d319397b30d1f3b
Veselin Ivanov <veso42srma@gmail.com>
2024-07-18 15:18:04 +0300

    added new test file

---
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_WebDriver_POM/PagesTests/HomePageLinks.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using StudentsRegistryPOM.Pages;
+
+namespace StudentsRegistryPOM.PagesTests
+{
+    public class HomePageLinks : BaseTests
+    {
+        [Test]
+        public void TestHomePageLinks()
+        {
+            var homePage = new HomePage(driver);
+            homePage.OpenPage();
+            Assert.IsTrue(homePage.IsPageOpen());
+
+            var addStudentPage = new AddStudentPage(driver);
+            addStudentPage.OpenPage();
+            Assert.IsTrue(addStudentPage.IsPageOpen());  
+
+            ViewStudentsPage viewStudentsPage = new ViewStudentsPage(driver);   
+            viewStudentsPage.OpenPage();
+            Assert.True(viewStudentsPage.IsPageOpen());
+        }
+
+    }
+}
