09a5b0410d9f21e8bb764efdb061b3148299e789
Veselin Ivanov <veso42srma@gmail.com>
2024-07-18 11:39:45 +0300

    new page added

---
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_WebDriver_POM/Pages/AddStudent.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace StudentsRegistryPOM.Pages
+{
+    public class AddStudent : BasePage // inheriting the basePage attributes
+    {
+        //ctor 
+        public AddStudent(IWebDriver driver) : base(driver)
+        {
+            // base(driver) points a refference to the inheritated BasePage driver field.
+        }
+    }
+}
