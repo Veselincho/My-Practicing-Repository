7d464dfb9b7ce3fb67a8eaebabcfb6a6b6bf7a96
Veselin Ivanov <veso42srma@gmail.com>
2024-07-24 16:33:37 +0300

    add including

--- FrontEnd_TestAutomation/Appium/noteAPP/AppNoteTests.cs
+++ FrontEnd_TestAutomation/Appium/noteAPP/AppNoteTests.cs
@@ -1,8 +1,13 @@
 global using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Android;
 using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.DevTools.V124.CSS;
 using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace noteAPP
 
@@ -12,9 +17,25 @@ namespace noteAPP
     {
         //C:\Users\VesoPC\Desktop\My-Practicing-Repository\FrontEnd_TestAutomation\Appium\apks\Notepad.apk
 
+        private WebDriverWait _wait;
         private AndroidDriver? _driver;
         private AppiumLocalService? _service;
         private AppiumOptions? _options;
+
+        //mapping locators:
+        public string menuButtonByID = "com.socialnmobile.dictapps.notepad.color.note:id/icon_nav";
+        public string addButtonByID = "com.socialnmobile.dictapps.notepad.color.note:id/main_btn1";
+        public string searchButtonByID = "com.socialnmobile.dictapps.notepad.color.note:id/main_btn2";
+        public string moreDropDownButtonByID = "com.socialnmobile.dictapps.notepad.color.note:id/main_btn3";
+        public string noteListViewByID = "com.socialnmobile.dictapps.notepad.color.note:id/note_list";
+        public string firstNoteElementByID = "com.socialnmobile.dictapps.notepad.color.note:id/title";
+        public string pencilEditButtonById = "com.socialnmobile.dictapps.notepad.color.note:id/edit_btn";
+        public string titleEditInputFieldById = "com.socialnmobile.dictapps.notepad.color.note:id/edit_title";
+        public string doneButtonById = "com.socialnmobile.dictapps.notepad.color.note:id/back_btn";
+        public string noteOptionsById = "com.socialnmobile.dictapps.notepad.color.note:id/menu_btn";
+        public string deleteNoteButtonByXPATH = "//android.widget.TextView[@resource-id='com.socialnmobile.dictapps.notepad.color.note:id/text' and @text='Delete']";
+        public string confirmDeleteButtonByID = "android:id/button1";
+
 
         [SetUp]
         public void SetUp()
@@ -33,7 +54,7 @@ namespace noteAPP
 
 
             // eensure there arent any pop up permissions shown and directly the app stars
-            _options.AddAdditionalAppiumOption("autoGrantPermissions", true); 
+            _options.AddAdditionalAppiumOption("autoGrantPermissions", true);
 
             _driver = new AndroidDriver(_service, _options);
 
@@ -47,7 +68,7 @@ namespace noteAPP
         public void TearDown()
         {
             _driver?.Quit();
-            _driver?.Dispose();   
+            _driver?.Dispose();
             _service?.Dispose();
         }
 
@@ -57,7 +78,7 @@ namespace noteAPP
         {
             var addNoteElement = _driver.FindElement(MobileBy.Id("com.socialnmobile.dictapps.notepad.color.note:id/img_add"));
             addNoteElement.Click();
-                
+
             //choosing the text element
             _driver.FindElement(MobileBy.XPath("//android.widget.TextView[@resource-id='com.socialnmobile.dictapps.notepad.color.note:id/text' and @text='Text']")).Click();
 
@@ -72,6 +93,46 @@ namespace noteAPP
 
             var result = _driver.FindElement(MobileBy.XPath("//android.widget.TextView[@resource-id='com.socialnmobile.dictapps.notepad.color.note:id/title' and @text='test']"));
             Assert.That(result.Text, Is.EqualTo("test"));
+        }
+
+        [Test]
+        public void AddandEditTextNote()
+        {
+            AddTextNote(); //using the first test to create a note
+            _driver.FindElement(MobileBy.Id(firstNoteElementByID)).Click();
+            _driver.FindElement(MobileBy.Id(pencilEditButtonById)).Click();
+            _driver.FindElement(MobileBy.Id(titleEditInputFieldById)).Clear();
+            _driver.FindElement(MobileBy.Id(titleEditInputFieldById)).SendKeys("Edited");
+            _driver.FindElement(MobileBy.Id(doneButtonById)).Click();
+            _driver.FindElement(MobileBy.Id(doneButtonById)).Click();   // backButton but same id 
+
+            var res = _driver.FindElement(MobileBy.Id(firstNoteElementByID));
+            var actualResult = res.Text;
+
+            Assert.AreEqual("Edited", actualResult);
+        }
+
+        [Test]
+        public void DeleteTextNote()
+        {
+            AddTextNote(); //using the first test to create a note
+            _driver.FindElement(MobileBy.Id(firstNoteElementByID)).Click();
+            _driver.FindElement(MobileBy.Id(noteOptionsById)).Click();
+            _driver.FindElement(MobileBy.XPath(deleteNoteButtonByXPATH)).Click();
+            _driver.FindElement(MobileBy.Id(confirmDeleteButtonByID)).Click();
+
+            // Assert that the element is not found
+            bool elementNotFound = false;
+            try
+            {
+                _driver.FindElement(MobileBy.Id(firstNoteElementByID));
+            }
+            catch (NoSuchElementException)
+            {
+                elementNotFound = true;
+            }
+
+            Assert.IsTrue(elementNotFound, "Element was found, but it should have been deleted.");
         }
     }
 }
