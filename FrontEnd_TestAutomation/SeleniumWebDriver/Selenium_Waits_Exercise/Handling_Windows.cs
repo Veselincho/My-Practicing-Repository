22d1d4a26087a7c7b037bb55eb246575177f6d52
Veselin Ivanov <veso42srma@gmail.com>
2024-07-27 13:26:39 +0300

    add tests HandleMultipleWindowsV2, handleNoSuchWindow

--- FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_Waits_Exercise/Handling_Windows.cs
+++ FrontEnd_TestAutomation/SeleniumWebDriver/Selenium_Waits_Exercise/Handling_Windows.cs
@@ -50,6 +50,65 @@ namespace Selenium_Waits_Exercise
                 //Expected exception aas the session is closed
             }
         }
-      
+
+        [Test]
+        public void HandleMultipleWindowsV2()
+        {
+            driver.FindElement(By.LinkText("Click Here")).Click();
+            var handles = driver.WindowHandles;
+
+            driver.SwitchTo().Window(handles[1]);
+            var h3Element = driver.FindElement(By.CssSelector("h3")).Text; // get its text directlyy
+
+            Assert.That(driver.PageSource, Contains.Substring(h3Element), "Page does not contain h3 element text");
+            Assert.That(h3Element, Is.EqualTo("New Window"), "h3 element text is not `New Window`");
+
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "content.txt");
+            if (File.Exists(path))
+            {
+                File.Delete(path);  
+            }
+            else
+            {
+                File.AppendAllText(path, "current handle: "+driver.CurrentWindowHandle);
+                File.AppendAllText(path, "page content: "+driver.PageSource);
+            }
+
+            driver.Close();
+            driver.SwitchTo().Window(handles.First());  // same as bottom
+                                                        //     driver.SwitchTo().Window(handles[0]);  
+            string originalWindowContent = driver.FindElement(By.CssSelector("h3")).Text;
+            Assert.That(originalWindowContent, Is.EqualTo("Opening a new window"));
+
+            // append the content from the first handle 
+            File.AppendAllText(path, "current handle: " + driver.CurrentWindowHandle);
+            File.AppendAllText(path, "page content: " + driver.PageSource);
+        }
+
+        [Test]
+        public void handleNoSuchWindow()
+        {
+            var handles = driver.WindowHandles;
+            driver.FindElement(By.LinkText("Click Here")).Click();
+            handles = driver.WindowHandles;
+
+            driver.SwitchTo().Window(handles[1]);
+            driver.Close();
+            driver.SwitchTo().Window(handles[0]);
+
+            try
+            {         
+                //try to visit closed window
+                driver.SwitchTo().Window(handles[1]);
+            }
+            catch (NoSuchWindowException ex)
+            {
+                Assert.Pass("catched the expected NoSuchWindowException: " + ex.Message); // catched the EXpected exception so the test passes
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail("unexpected except: " + ex.Message); //marks the test as failed 'cuz of UNexpected exception caugh
+            }
+        }
     }
 }
