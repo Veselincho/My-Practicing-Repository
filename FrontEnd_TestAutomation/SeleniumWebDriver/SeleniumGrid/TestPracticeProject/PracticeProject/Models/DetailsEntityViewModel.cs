2212c6f4333700577def14241edab55f1abd318b
Veselin Ivanov <veso42srma@gmail.com>
2024-07-28 23:00:48 +0300

    Remove PracticeProject folder

--- FrontEnd_TestAutomation/SeleniumWebDriver/SeleniumGrid/TestPracticeProject/PracticeProject/Models/DetailsEntityViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using PracticeProject.Models;
-
-namespace PracticeProject.Services
-{
-    public class DetailsEntityViewModel : EntityViewModel
-    {
-        public string Id { get; set; }
-    }
-}
