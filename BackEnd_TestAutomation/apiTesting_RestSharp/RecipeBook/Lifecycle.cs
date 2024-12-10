06c802cfdbec3f37d239e88bbb3d63bff6da90bc
Veselincho <veso42srma@gmail.com>
2024-12-10 21:20:30 +0200

    added category lifecycle test

---
+++ BackEnd_TestAutomation/apiTesting_RestSharp/RecipeBook/Lifecycle.cs
@@ -0,0 +1,50 @@
+﻿using RecipeBook;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace RecipeBook_CategoryManagement
+{
+    [TestFixture]
+    public class Lifecycle
+    {
+        private RestClient client;
+        private string token;
+        private int randomNum;
+        private string lastRandom;
+        private string category;
+
+        [SetUp]
+        public void SetUp()
+        {
+            client = new RestClient(GlobalConstants.BaseUrl);
+            token = GlobalConstants.AuthenticateUser("john.doe@example.com", "password123");
+
+            var random = new Random();
+            randomNum = random.Next(10, 50000);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            client.Dispose();
+        }
+
+        [Test]
+        public void Test_CategoryLifecycle()
+        {
+            //[1]: Create new Category
+            var randomName = $"name_{randomNum}";
+            var postReq = new RestRequest("/category", Method.Post)
+                .AddHeader("Authorization", $"Bearer {token}")
+                .AddJsonBody(new
+                {
+                    name = randomName
+                });
+
+            var postResponse = client.Execute(postReq);
+
+            Assert.True(postResponse.IsSuccessful);
+        }
+
+    }
+}
